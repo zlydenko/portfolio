fbf002d4f20797ba56b401d1fe20a9b0919a3471
zlydenko <anton.zlydenko@gmail.com>
2018-03-25 11:00:00 +0300

    bye pattern, you useless bastard

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@ import React from "react";
 import styled from "styled-components";
 
 import Header from "./Header";
-import Pattern from "./Pattern";
 
 export default function App() {
   return (
     <React.Fragment>
-      <Pattern bg={"rgb(255,255,255)"} opacity={1} color={"rgba(242,2,10,0.7)"}>
-        <Header />
-      </Pattern>
+      <Header />
     </React.Fragment>
   );
 }
