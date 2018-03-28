4ea47072aa611591d275315afa40239b59583d1f
zlydenko <anton.zlydenko@gmail.com>
2018-03-28 12:05:00 +0300

    finish about section

--- src/About.js
+++ src/About.js
@@ -3,13 +3,27 @@ import styled from "styled-components";
 
 import { about } from "./content";
 
-import ParagraphWithDropCap from "./ParagraphWithDropCap";
+const Text = styled.p`
+  font-size: 1.8em;
+  font-family: "Fira Mono", monospace;
+  font-weight: 400;
+  letter-spacing: -0.04em;
+
+  @media (min-width: 768px) {
+    font-size: 2.4em;
+  }
+`;
+const SmallText = styled.p`
+  font-size: 1.4em;
+
+  text-shadow: 0.04em 0em gold;
+`;
 
 const About = () => (
   <Fragment>
-    <ParagraphWithDropCap content={about.firstPar} />
-    <p>{about.caption}</p>
-    <p>{about.secondPar}</p>
+    <Text>{about.firstPar} </Text>
+    <SmallText>{about.caption}</SmallText>
+    <Text>{about.secondPar}</Text>
   </Fragment>
 );
 
