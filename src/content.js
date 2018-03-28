404bcd6ad1c01dfbb8ed661c660c742696676e06
zlydenko <anton.zlydenko@gmail.com>
2018-03-28 11:32:00 +0300

    small fixes

--- src/content.js
+++ src/content.js
@@ -1,5 +1,5 @@
 export const about = {
-  firstPar: `Hi! My name is Anton. Using algorithmic and structured thinking I'm translate human needs on machine's language with access for people across the globe.`,
+  firstPar: `Hello! My name is Anton. Using algorithmic and structured thinking I'm translate human needs on machine's language with access for people across the globe.`,
   caption: `Some call it frontend development nowadays`,
   secondPar: `For me — it's more about curiosity, love to web technologies and interest in "how things work" and "how to create things that work".`
 };
