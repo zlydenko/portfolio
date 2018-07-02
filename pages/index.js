c7f06e43a766a8655974f92111b8c4a2a863d75a
anton zlydenko <anton.zlydenko@gmail.com>
2018-07-02 15:07:52 +0300

    add layout grid from flexboxgrid2

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,22 @@
+import Head from "next/head";
+
 import Background from "../components/Background";
 import About from "../components/About";
 
 export default () => (
   <div>
+    <Head>
+      <title>fancy page</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <link
+        rel="stylesheet"
+        href="https://unpkg.com/flexboxgrid2@7.1.0/flexboxgrid2.css"
+      />
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Fira+Mono|Open+Sans"
+      />
+    </Head>
     <Background />
     <About />
     <style global jsx>
@@ -18,6 +32,7 @@ export default () => (
         }
 
         body {
+          font-family: "Open Sans", sans-serif;
           margin: 0;
           padding: 0;
         }
