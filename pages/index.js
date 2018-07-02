7fd6b822ad670d466c2542961c8f7f4f3cebc401
anton zlydenko <anton.zlydenko@gmail.com>
2018-07-02 14:35:09 +0300

    add particles background

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,8 @@
-export default () => <div>hello world</div>;
+import Background from "../components/Background";
+
+export default () => (
+  <div>
+    <Background />
+    <h1>hello world</h1>
+  </div>
+);
