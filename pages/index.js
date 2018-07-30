062055a71c7d15dd67da8f7e9e532c2343960848
anton zlydenko <anton.zlydenko@gmail.com>
2018-07-30 13:55:15 +0300

    add favicons & manifest

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ import Footer from "../components/Footer";
 export default () => (
   <div>
     <Head>
-      <title>fancy page</title>
+      <title>Anton Zlydenko</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <link
         rel="stylesheet"
@@ -18,6 +18,36 @@ export default () => (
         href="https://fonts.googleapis.com/css?family=Fira+Mono:400,700|Poppins:400,900"
         rel="stylesheet"
       />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/static/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/static/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/static/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/static/site.webmanifest" />
+      <link
+        rel="mask-icon"
+        href="/static/safari-pinned-tab.svg"
+        color="#5bbad5"
+      />
+      <meta
+        name="apple-mobile-web-app-title"
+        content="Anton Zlydenko portfolio"
+      />
+      <meta name="application-name" content="Anton Zlydenko portfolio" />
+      <meta name="msapplication-TileColor" content="#2b5797" />
+      <meta name="theme-color" content="#ffffff" />
     </Head>
     <Background />
     <div className="container">
